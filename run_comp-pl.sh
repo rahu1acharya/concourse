2163a2f77c09f8cbbd9da8282110717d8fc6342e
rahu1acharya <167766966+rahu1acharya@users.noreply.github.com>
2024-08-29 17:52:14 +0530

    Update run_comp-pl.sh

--- run_comp-pl.sh
+++ run_comp-pl.sh
@@ -18,9 +18,9 @@ cd py-script
 # Run the scrape.py script
 python comp-pl.py
 
-# Move the output CSV file to the scraped-data directory
-mv all_companies_profit_loss.csv ../scraped-data/
+# # Move the output CSV file to the scraped-data directory
+# mv all_companies_profit_loss.csv ../scraped-data/
 
-# Print the contents of the CSV file
-echo "Contents of all_companies_profit_loss.csv in scraped-data directory:"
-cat ../scraped-data/all_companies_profit_loss.csv
+# # Print the contents of the CSV file
+# echo "Contents of all_companies_profit_loss.csv in scraped-data directory:"
+# cat ../scraped-data/all_companies_profit_loss.csv
