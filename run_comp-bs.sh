fbd7383a8ad74b8fa1b7f26b856f41486554bc18
rahu1acharya <167766966+rahu1acharya@users.noreply.github.com>
2024-09-04 10:45:01 +0530

    Create run_comp-bs.sh

---
+++ run_comp-bs.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+chmod +x py-script/run-scrape.sh
+# Install dependencies from requirements.txt
+pip install -r py-script/requirements.txt
+
+# Display the contents of secrets.env
+echo "Contents of secrets.env:"
+cat secrets-output/secrets.env
+
+# Source environment variables
+set -a
+. secrets-output/secrets.env
+set +a
+
+# Navigate to the directory containing scrape.py
+cd py-script
+
+# Run the scrape.py script
+python comp-bs.py
