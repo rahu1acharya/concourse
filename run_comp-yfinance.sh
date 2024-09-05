4da1bf29fca06174b38a23334e029fe00255575f
Rahul Acharya <167766966+rahu1acharya@users.noreply.github.com>
2024-09-05 16:40:09 +0530

    Create run_comp-yfinance.sh

---
+++ run_comp-yfinance.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+chmod +x py-script/run_comp-yfinance.sh
+# Install dependencies from requirements.txt
+pip install -r py-script/yfrequirements.txt
+
+# Navigate to the directory containing scrape.py
+cd py-script
+
+# Run the scrape.py script
+python yfinanceapi.py
